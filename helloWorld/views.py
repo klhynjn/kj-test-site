94f3f8015b07a992a1d6da3e43950eccbfbd7555
Kilhyun Jun <kilhyun.jun@bouncex.com>
2020-05-19 09:20:11 -0400

    use django.db to query

--- helloWorld/views.py
+++ helloWorld/views.py
@@ -1,11 +1,10 @@
 # from django.shortcuts import render
-
-from mysite.database import db_read_single_value
+from mysite.database import my_custom_sql
 
 # Create your views here.
 from django.http import HttpResponse
 def index(request):
-    database=db_read_single_value("select current_database()")
-    timestamp=db_read_single_value("select clock_timestamp()")
+    database=my_custom_sql("select current_database()")
+    timestamp=my_custom_sql("select clock_timestamp()")
     response=database+timestamp
     return HttpResponse(response)
--- mysite/database.py
+++ mysite/database.py
@@ -1,9 +1,7 @@
-import psycopg2
+from django.db import connection
 
-def db_read_single_value( sql ) :
-    conn = psycopg2.connect('')
-    cur = conn.cursor()
-    cur.execute(sql)
-    row = cur.fetchone()
-    conn.close()
+def my_custom_sql( sql ):
+    with connection.cursor() as cursor:
+        cursor.execute(sql)
+        row = cursor.fetchone()
     return row
